56774075c0113d0ee437c54015b9241688b575e8
Puneet Gupta <puneetg@microsoft.com>
2018-03-22 19:19:30 +0530

    Adding crashing pages

---
+++ WebCampGit/StackOverflowPage.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demomvp
+{
+    public partial class StackOverflowPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ThisIsARecursiveFunctionUsedToTriggerAStackOVerflow();
+        }
+
+        private void ThisIsARecursiveFunctionUsedToTriggerAStackOVerflow()
+        {
+            try
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    ThisIsARecursiveFunctionUsedToTriggerAStackOVerflow();
+                }
+            }
+            catch (StackOverflowException ex)
+            {
+                Response.Write(ex.Message + "<-*******->" + ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
---
+++ WebCampGit/UnhandledException.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demomvp
+{
+    public partial class UnhandledException : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ThreadStart ts = new ThreadStart(Crashme);
+            System.Threading.Thread t = new System.Threading.Thread(ts);
+            t.Start();
+        }
+
+        void Crashme()
+        {
+            throw new ApplicationException("I will crash your process");
+        }
+    }
+}
